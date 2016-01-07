8f287aca995b453d449ca17bb8d219439fa4ed4a
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-01-06 20:17:32 -0700

    fota .zip

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -87,6 +87,7 @@ set(NRF51822_SOFTDEVICE_HEX_FILE "${NRF51822_SOFTDEVICE_FILE_PATH}")
 set(NRF51822_BOOTLOADER_HEX_FILE "${CMAKE_CURRENT_LIST_DIR}/../bootloader/s130_nrf51_1.0.0_bootloader.hex")
 set(NRF51822_MERGE_HEX_SCRIPT    "${CMAKE_CURRENT_LIST_DIR}/../scripts/merge_hex.py")
 set(NRF51822_MEMORY_INFO_SCRIPT  "${CMAKE_CURRENT_LIST_DIR}/../scripts/memory_info.py")
+set(NRF51822_GEN_DAT_SCRIPT      "${CMAKE_CURRENT_LIST_DIR}/../scripts/generate_dat.py")
 
 # define a function for yotta to apply target-specific rules to build products,
 # in our case we need to convert the built elf file to .hex, and add the
@@ -98,6 +99,11 @@ function(yotta_apply_target_rules target_type target_name)
                 POST_BUILD
                 # objcopy to hex
                 COMMAND arm-none-eabi-objcopy -O ihex ${target_name} ${target_name}.hex
+                # objcopy to bin
+                COMMAND arm-none-eabi-objcopy -O binary ${target_name} ${target_name}.bin
+                # generate dfu .dat from bin
+                COMMAND python ${NRF51822_GEN_DAT_SCRIPT} ${target_name}.bin
+                COMMENT "generating .dat and .zip"
                 # and append the softdevice hex file
                 COMMAND python ${NRF51822_MERGE_HEX_SCRIPT} ${NRF51822_SOFTDEVICE_HEX_FILE} ${target_name}.hex ${target_name}-combined.hex
                 # append the softdevice and bootloader hex file
