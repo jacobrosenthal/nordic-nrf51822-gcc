c68a82e98f18440f2048eb5b8b1b13f2fd3f919b
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-04-30 00:53:02 -0400

    add sd-req dev-revision and dev-type

--- scripts/generate_dat.py
+++ scripts/generate_dat.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-# genereate .dat and .zip for an application binary
-# todo, take softdevice and bootloaders and do whatever alterations to the hex necesary 
+# generate .dat and .zip for an application binary
 # https://github.com/NordicSemiconductor/nRF-Master-Control-Panel/blob/master/init%20packet%20handling/How%20to%20generate%20the%20INIT%20file%20for%20DFU.pdf
-# code shamelessly stolen from https://devzone.nordicsemi.com/question/22586/anyone-do-successfully-dfu-ota-for-sdk701-softdevice-s110-v710/
 
+# todo, align more closely with the nordic tool. Take in hex as well/instead of ints. take in softdevice and application should come in via --application
 
 import argparse, sys, zipfile, glob, os
 
+### shamelessly stolen from https://devzone.nordicsemi.com/question/22586/anyone-do-successfully-dfu-ota-for-sdk701-softdevice-s110-v710/
 def calc_crc16(binfile):
   crc = 0xFFFF
 
@@ -22,22 +22,10 @@ def calc_crc16(binfile):
 def convert_uint16_to_array(value):
     """ Convert a number into an array of 2 bytes (LSB). """
     return [(value >> 0 & 0xFF), (value >> 8 & 0xFF)]
+###
 
-def _create_init_packet(init_packet):
-    hardware_version = [0xFF, 0xFF]
-    hardware_revision = [0xFF, 0xFF]
-    application_version = [0xFF, 0xFF, 0xFF, 0xFF]
-    softdevice_len = [0x01, 0x00]
-    softdevice_array = [0xFE, 0xFF]
-
-    init_packet.extend(hardware_version)
-    init_packet.extend(hardware_revision)
-    init_packet.extend(application_version)
-    init_packet.extend(softdevice_len)
-    init_packet.extend(softdevice_array)
-
-def _create_manifest(bin_file, dat_file, application_version, device_revision, device_type, firmware_crc16, softdevice_req):
-  manifest_text = """
+def _create_manifest(bin_file, dat_file, dev_type, dev_revision, application_version, sd_req, crc):
+  manifest = """
   {
     "manifest": {
         "application": {
@@ -56,20 +44,32 @@ def _create_manifest(bin_file, dat_file, application_version, device_revision, d
         "dfu_version": 0.5
     }
 }"""
-  return manifest_text % (bin_file, dat_file, application_version, device_revision, device_type, firmware_crc16, softdevice_req)
+  return manifest % (bin_file, dat_file, application_version, dev_revision, dev_type, crc, sd_req)
 
 def main():
 
   parser = argparse.ArgumentParser(description='generate a .bin.dat file from an input bin file.')
-  parser.add_argument ('file', metavar = 'file', type = str, help = 'The binary to crc')
+  parser.add_argument ('file', type = str, help = 'The binary to crc')
+  parser.add_argument ('--dev-type', default=65535, type = int, help = 'The device type as an integer, ex 65535 for 0xFFFF for all devices')
+  parser.add_argument ('--dev-revision', default=65535, type = int, help = 'The revision as in integer, ex 65535 for 0xFFFF for all revisions')
+  parser.add_argument ('--sd-req', default=65534, type = int, help = 'The required softdevice (singular) as in integer, ex 100 for 0x64 for SD8.0')
+  parser.add_argument ('--application-version', default=4294967295, type = int, help = 'The application version as in integer, ex 4294967295 for 0xFFFFFFFF for all versions')
+
   args = parser.parse_args()
 
-  name = args.file.split('.')[0]
+  basename = os.path.basename(args.file)
+  name = basename.split('.')[0]
 
-  dat_out_name =  name + '.dat'
   manifest_name = 'manifest.json'
-  archive_name = name + '.zip'
-  bin_out_name = name + '.bin'
+
+  #if were not in the same directory
+  if ('/' in args.file) or ('\\' in args.file):
+    path = os.path.dirname(args.file)
+    archive_name = path + '/' + name + '.zip'
+    dat_out_name = path + '/' + name + '.dat'
+  else:
+    archive_name = name + '.zip'
+    dat_out_name = name + '.dat'
 
   try:
     bin = open(args.file, 'rb')
@@ -78,47 +78,29 @@ def main():
     sys.exit('Could not open %s' % args.file)
 
   try:
-    dat = open(dat_out_name, 'w')
+    archive = zipfile.ZipFile( archive_name, 'w')
 
   except:
-    sys.exit('Could not open/create %s' % dat_out_name)
-
-  try:
-    manifest = open(manifest_name, 'w')
-
-  except:
-    sys.exit('Could not open/create %s' % manifest_name)
-
-  try:
-    archive = zipfile.ZipFile(archive_name, 'w')
-
-  except:
+    bin.close();
     sys.exit('Could not open/create %s' % archive_name)
 
-  init_packet = []
-  _create_init_packet(init_packet)
-
   ba = bytearray(bin.read())
+  crc = calc_crc16(ba)
   bin.close();
 
-  crc = calc_crc16(ba)
+  init_packet = []
+  init_packet.extend([args.dev_type & 0xFF, (args.dev_type >> 8) & 0xFF])
+  init_packet.extend([args.dev_revision & 0xFF, (args.dev_revision >> 8) & 0xFF])
+  init_packet.extend([args.application_version & 0xFF, (args.application_version >> 8) & 0xFF, (args.application_version >> 16) & 0xFF, (args.application_version >> 24) & 0xFF])
+  init_packet.extend([0x01, 0x00])
+  init_packet.extend([args.sd_req & 0xFF, (args.sd_req >> 8) & 0xFF])
   init_packet.extend(convert_uint16_to_array(crc))
 
-  dat.write(bytearray(init_packet))
-  dat.close()
+  manifest = _create_manifest(basename, dat_out_name, args.dev_type, args.dev_revision, args.application_version, args.sd_req, crc)
 
-  application_version = 4294967295
-  device_revision = 65535
-  device_type = 65535
-  softdevice_req = 65534
-
-  manifest_text = _create_manifest(bin_out_name, dat_out_name, application_version, device_revision, device_type, crc, softdevice_req)
-  manifest.write(manifest_text);
-  manifest.close()
-
-  archive.write(dat_out_name, os.path.basename(dat_out_name), zipfile.ZIP_DEFLATED)
-  archive.write(manifest_name, os.path.basename(manifest_name), zipfile.ZIP_DEFLATED)
-  archive.write(args.file, os.path.basename(bin_out_name), zipfile.ZIP_DEFLATED)
+  archive.writestr(dat_out_name, buffer(bytearray(init_packet)), zipfile.ZIP_DEFLATED)
+  archive.writestr(manifest_name, manifest, zipfile.ZIP_DEFLATED)
+  archive.write(args.file, basename, zipfile.ZIP_DEFLATED)
   archive.close()
 
 if __name__ == "__main__":
