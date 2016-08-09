1bcee9e9cfbea9b78b8d96273bb51c13c91bfb32
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-08-09 12:24:21 -0400

    fix sd_req default and refactor for include as well as cli

--- scripts/generate_dat.py
+++ scripts/generate_dat.py
@@ -46,36 +46,27 @@ def _create_manifest(bin_file, dat_file, dev_type, dev_revision, application_ver
 }"""
   return manifest % (bin_file, dat_file, application_version, dev_revision, dev_type, crc, sd_req)
 
-def main():
+def zipfrombin(file, dev_type = 65535, dev_revision = 65535, application_version = 4294967295, sd_req = 65535):
 
-  parser = argparse.ArgumentParser(description='generate a .bin.dat file from an input bin file.')
-  parser.add_argument ('file', type = str, help = 'The binary to crc')
-  parser.add_argument ('--dev-type', default=65535, type = int, help = 'The device type as an integer, ex 65535 for 0xFFFF for all devices')
-  parser.add_argument ('--dev-revision', default=65535, type = int, help = 'The revision as in integer, ex 65535 for 0xFFFF for all revisions')
-  parser.add_argument ('--sd-req', default=65534, type = int, help = 'The required softdevice (singular) as in integer, ex 100 for 0x64 for SD8.0')
-  parser.add_argument ('--application-version', default=4294967295, type = int, help = 'The application version as in integer, ex 4294967295 for 0xFFFFFFFF for all versions')
-
-  args = parser.parse_args()
-
-  basename = os.path.basename(args.file)
+  basename = os.path.basename(file)
   name = basename.split('.')[0]
 
   manifest_name = 'manifest.json'
 
   #if were not in the same directory
-  if ('/' in args.file) or ('\\' in args.file):
-    path = os.path.dirname(args.file)
+  if ('/' in file) or ('\\' in file):
+    path = os.path.dirname(file)
     archive_name = path + '/' + name + '.zip'
-    dat_out_name = path + '/' + name + '.dat'
   else:
     archive_name = name + '.zip'
-    dat_out_name = name + '.dat'
+
+  dat_out_name = name + '.dat'
 
   try:
-    bin = open(args.file, 'rb')
+    bin = open(file, 'rb')
 
   except:
-    sys.exit('Could not open %s' % args.file)
+    sys.exit('Could not open %s' % file)
 
   try:
     archive = zipfile.ZipFile( archive_name, 'w')
@@ -89,19 +80,31 @@ def main():
   bin.close();
 
   init_packet = []
-  init_packet.extend([args.dev_type & 0xFF, (args.dev_type >> 8) & 0xFF])
-  init_packet.extend([args.dev_revision & 0xFF, (args.dev_revision >> 8) & 0xFF])
-  init_packet.extend([args.application_version & 0xFF, (args.application_version >> 8) & 0xFF, (args.application_version >> 16) & 0xFF, (args.application_version >> 24) & 0xFF])
+  init_packet.extend([dev_type & 0xFF, (dev_type >> 8) & 0xFF])
+  init_packet.extend([dev_revision & 0xFF, (dev_revision >> 8) & 0xFF])
+  init_packet.extend([application_version & 0xFF, (application_version >> 8) & 0xFF, (application_version >> 16) & 0xFF, (application_version >> 24) & 0xFF])
   init_packet.extend([0x01, 0x00])
-  init_packet.extend([args.sd_req & 0xFF, (args.sd_req >> 8) & 0xFF])
+  init_packet.extend([sd_req & 0xFF, (sd_req >> 8) & 0xFF])
   init_packet.extend(convert_uint16_to_array(crc))
 
-  manifest = _create_manifest(basename, dat_out_name, args.dev_type, args.dev_revision, args.application_version, args.sd_req, crc)
+  manifest = _create_manifest(basename, dat_out_name, dev_type, dev_revision, application_version, sd_req, crc)
 
   archive.writestr(dat_out_name, buffer(bytearray(init_packet)), zipfile.ZIP_DEFLATED)
   archive.writestr(manifest_name, manifest, zipfile.ZIP_DEFLATED)
-  archive.write(args.file, basename, zipfile.ZIP_DEFLATED)
+  archive.write(file, basename, zipfile.ZIP_DEFLATED)
   archive.close()
+
+def main():
+
+  parser = argparse.ArgumentParser(description='generate a .bin.dat file from an input bin file.')
+  parser.add_argument ('file', type = str, help = 'The binary to crc')
+  parser.add_argument ('--dev-type', default=65535, type = int, help = 'The device type as an integer, ex 65535 for 0xFFFF for all devices')
+  parser.add_argument ('--dev-revision', default=65535, type = int, help = 'The revision as in integer, ex 65535 for 0xFFFF for all revisions')
+  parser.add_argument ('--sd-req', default=65535, type = int, help = 'The required softdevice (singular) as in integer, ex 100 for 0x64 for SD8.0')
+  parser.add_argument ('--application-version', default=4294967295, type = int, help = 'The application version as in integer, ex 4294967295 for 0xFFFFFFFF for all versions')
+
+  args = parser.parse_args()
+  zipfrombin(args.file, args.dev_type, args.dev_revision, args.application_version, args.sd_req)
 
 if __name__ == "__main__":
   main()
